2aa22123f34aa91232b76a65ed1186fe07be933c
edward nayve <edward.nayve@boom.camp>
2021-07-09 20:32:51 +0800

    setup express server

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
+const express = require("express")
 const env = require('dotenv').config()
+const app = express()
+
+var PORT = process.env.PORT || 3000
 
 const command = require("./Commands/command")
 const pong = require("./Commands/pong")
 
-console.log("Bot is now live")
+
 
 const aliases = ['nayvs', 'ed', 'ward']
 
-client.on("ready", () => {
-    console.log(`Bot is currently running on version v${require('./package.json').version}`)
+app.listen(PORT, () => {
+    console.log("Bot is now live")
+    client.on("ready", () => {
+        console.log(`Bot is currently running on version v${require('./package.json').version}`)
 
-    command(client, aliases, message => {
-        pong(message)
+        command(client, aliases, message => {
+            pong(message)
+        })
     })
-})
 
-client.login(process.env.BOTTOKEN)
+    client.login(process.env.BOTTOKEN)
+})
